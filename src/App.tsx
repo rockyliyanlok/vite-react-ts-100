b230ddc83bd0c3819bf745e195046e618aa83416
yli <yli@bol.com>
2022-08-22 20:19:19 +0200

    3-improve-performance-metric

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,29 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const logoProps = {
+    width: 96,
+    height: 96,
+  }
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img
+            src="/vite.svg"
+            className="logo"
+            alt="Vite logo"
+            {...logoProps}
+          />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            {...logoProps}
+          />
         </a>
       </div>
       <h1>Vite + React + TS 100%</h1>
